4be3ffaf92e20f13f5c62a0d8be3216ee7b36096
ayushman1729 <133388754+ayushman1729@users.noreply.github.com>
2024-11-24 18:44:30 +0000

    conversion od datatypes

---
+++ 01_basic/03_conversionOperator.js
@@ -0,0 +1,28 @@
+let score="33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+let valueInNumber=Number(score);  //string to number
+console.log(typeof valueInNumber)
+
+//"33" => 33
+//"33abc"=> Nan
+//true=>1 ; false => 0
+
+let isLoggedIn=1;
+
+let booleanIsLoggedin=Boolean(isLoggedIn);
+console.log(booleanIsLoggedin)
+
+//1 => true; false=>0
+//"=>false"
+//"ayush"=> true
+
+let someNumber =33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);   //33 is now string 
+console.log(typeof stringNumber);
+
