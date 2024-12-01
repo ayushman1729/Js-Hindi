428cfb1ec9aa40a72570a6593850ae18d7cdff57
ayushman1729 <133388754+ayushman1729@users.noreply.github.com>
2024-12-01 11:42:06 +0000

    03_objects

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,38 @@
+//singleton
+
+
+//object literals
+const mySym=Symbol("key1")
+
+const JsUSer={
+    name:"Hitsh",
+    "full name": "hitesh chaudhary",  // console.log(JsUSer["full name"]); 
+   [mySym]:"mykey1", //symbol h console.log(JsUSer[mySym]);
+    ave:18,
+    location:"lucknow",
+    email:"hitesh@google.com",
+    lastlogindays:["Monday","Saturday"]
+
+}
+
+// console.log(JsUSer.email);
+// console.log(JsUSer["email"]);
+// console.log(JsUSer["full name"]);
+// console.log(typeof JsUSer.mySym);
+console.log(JsUSer[mySym]); //symbol reprent to print
+
+JsUSer.email="hitesh@go000000000ogle.com"
+// Object.freeze(JsUSer)  //use for freeze 
+JsUSer.email="hitesh@microsoft.com"
+
+// console.log(JsUSer);
+
+JsUSer.greeting= function(){
+    console.log("HELLO JS USER");   
+}
+
+JsUSer.greetingTwo= function(){
+    console.log(`HELLO JS USER, ${this.name}`);   
+}
+console.log(JsUSer.greeting());
+console.log(JsUSer.greetingTwo());
