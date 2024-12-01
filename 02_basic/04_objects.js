83aef1d2b55f2ba52b1b22094b9d5ce51594ba88
ayushman1729 <133388754+ayushman1729@users.noreply.github.com>
2024-12-01 14:31:22 +0000

    04_objects.js

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,59 @@
+// const tinderuser= new Object()  //singleton object
+
+const tinderUser= {}   //non single ton object
+
+tinderUser.id= "123abc"
+tinderUser.name="Sammy"
+tinderUser.isLoggedIn=false
+
+console.log(tinderUser); //all values print
+
+// console.log(Object.keys(tinderUser));  //[ 'id', 'name', 'isLoggedIn' ]
+
+// console.log(Object.values(tinderUser));  //[ '123abc', 'Sammy', false ]
+
+// console.log(Object.entries(tinderUser)); //sbko array mei bana deta h [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@google.com",
+  fullname: {
+    userfullname: {
+      firstname:"ayushman",
+      lastname:"singh"
+    }
+  }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+const users = [
+  {
+      id: 1,
+      email: "h1@gmail.com"
+  },
+  {
+      id: 1,
+      email: "h2@gmail.com"
+  },
+  {
+      id: 1,
+      email: "h3@gmail.com"
+  },
+]
+
+
+// console.log(users[0].email);
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn')); //ye property h ya nhi 
