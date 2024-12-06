d9300b55eeff0b154d4abce136f4d131df60203d
ayushman1729 <133388754+ayushman1729@users.noreply.github.com>
2024-12-06 08:15:49 +0000

    03_conversionOperator.js

--- 01_basic/03_conversionOperator.js
+++ 01_basic/03_conversionOperator.js
@@ -1,11 +1,12 @@
-let score="33";
+let score="33"; //string
+let num= 33;
 
-console.log(typeof score);
-console.log(typeof(score));
+//  console.log(typeof(score)); //string 
+//  console.log(typeof num);  //num
 
-
-let valueInNumber=Number(score);  //string to number
-console.log(typeof valueInNumber)
+let valueInNumber=Number(score);  //string to number  actual value is this (NaN)
+let valueInNumber2=score;
+//console.log(typeof valueInNumber2)
 
 //"33" => 33
 //"33abc"=> Nan
@@ -13,16 +14,49 @@ console.log(typeof valueInNumber)
 
 let isLoggedIn=1;
 
-let booleanIsLoggedin=Boolean(isLoggedIn);
-console.log(booleanIsLoggedin)
+let booleanIsLoggedin=Boolean(isLoggedIn); //
+//console.log(booleanIsLoggedin)
 
 //1 => true; false=>0
-//"=>false"
+//" "=>false"
 //"ayush"=> true
 
 let someNumber =33
 
 let stringNumber=String(someNumber)
-console.log(stringNumber);   //33 is now string 
-console.log(typeof stringNumber);
+//console.log(stringNumber);   //33 is now string 
+//console.log(typeof stringNumber);
 
+
+// *****************Operations/**************
+
+let value=3;
+let negValue= -value;
+
+// console.log(negValue)
+// console.log("1"+ 2); //12
+// console.log(1+"2");  //12
+// console.log("1"+2+2); //122
+
+// console.log(3+ 2 + "2"); //52 num+string
+
+// console.log(true +2) //3
+
+// console.log(+true) //1
+
+// let a=3;
+// const b=a++;
+// console.log(`a: ${a}, b: ${b}`);
+// ///console.log(`x:${x}, y:${y}`);
+
+// let x=4;
+// const y=++x;
+// console.log(`x: ${x}, y:${y}`);
+
+let a=14;
+const b=--a
+console.log(`a:${a}, b:${b}`);
+
+let x=41;
+const y=x--
+console.log(`x:${x}, y:${y}`);
